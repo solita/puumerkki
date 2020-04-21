4e7b3f3aa6a4d03bb24961b83746cb3d3e158eb7
Aki Helin <aki.helin@iki.fi>
2020-04-21 09:22:13 +0300

    sorted deps. ring turned out to be sufficient for test server use.

--- project.clj
+++ project.clj
@@ -2,17 +2,14 @@
   :description "Puumerkki allekirjoituskirjasto ja esimerkki"
   :min-lein-version "2.9.1"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.json "1.0.0"]
-                 ;[metosin/compojure-api "1.2.0-20161028.114958-3"]
-                 ;[metosin/ring-swagger "0.22.11"]
-                 [ring/ring "1.7.1"]
                  [pandect "0.6.1"] ;; SHA
-                 [ring/ring-defaults "0.3.2"]
-                 ;[org.apache.pdfbox/pdfbox "1.8.16"]
-                 [org.apache.pdfbox/pdfbox "2.0.19"]
+                 ;[org.apache.pdfbox/pdfbox "1.8.16"] ;; no longer supported due to api changes required for 2.x
+                 [org.apache.pdfbox/pdfbox "2.0.19"] 
                
-                 ;; for internal test server 
+                 ;; for internal test server
+                 [ring/ring "1.7.1"]
                  [ring/ring-core "1.6.3"]
+                 [ring/ring-defaults "0.3.2"]
                  [hiccup "1.0.5"]
                  [ring/ring-jetty-adapter "1.6.3"]
                  [org.clojure/data.json "1.0.0"]]
