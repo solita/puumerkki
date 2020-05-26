78987fd9682aa4d5bc7dbaa5ed38f2c7eb2ea1c3
Aki Helin <aki.helin@iki.fi>
2020-05-26 13:43:32 +0300

    Bumped version to 0.9.2. Included signature options closing, which seems to fix a premature pdf closing issue.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject puumerkki "0.9.1"
+(defproject puumerkki "0.9.2"
   :description "Puumerkki allekirjoituskirjasto ja esimerkkipalvelin"
   :min-lein-version "2.9.1"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [pandect "0.6.1"] ;; SHA
                  ;[org.apache.pdfbox/pdfbox "1.8.16"] ;; no longer supported due to api changes required for 2.x
-                 [org.apache.pdfbox/pdfbox "2.0.19"] 
-               
+                 [org.apache.pdfbox/pdfbox "2.0.19"]
+
                  ;; for internal test server
                  [ring/ring "1.7.1"]
                  [ring/ring-core "1.6.3"]
