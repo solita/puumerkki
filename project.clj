9c7863881e5b4b514e2f44cab5086e003e460f8c
Antti Möttönen <antti.mottonen@solita.fi>
2024-06-11 16:48:49 +0300

    Deploy  under personal repository

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puumerkki "0.9.3-SNAPSHOT"
+(defproject io.github.solita-antti-mottonen/puumerkki "0.9.3-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/solita/puumerkki"
