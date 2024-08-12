6acf565312151f49044affea9f1af01a3a3940e9
Antti Möttönen <antti.mottonen@solita.fi>
2024-08-12 13:42:27 +0300

    Use newer dependency-check-core to fix NVD check

--- nvd/project.clj
+++ nvd/project.clj
@@ -1,5 +1,10 @@
 (defproject nvd-helper "local"
             :description "nvd-clojure helper project"
-            :dependencies [[nvd-clojure "4.0.0"]
+            :dependencies [[nvd-clojure "4.0.0"
+                            ;; Replaced by a newer version until NVD-Clojure is updated
+                            :exclusions [org.owasp/dependency-check-core]]
+                           [org.owasp/dependency-check-core "10.0.3"]
+                           ;; Dependency-check-core brings in older version which doesn't work
+                           [org.slf4j/slf4j-api "2.0.10"]
                            [org.clojure/clojure "1.11.3"]]
             :jvm-opts ["-Dclojure.main.report=stderr"])
